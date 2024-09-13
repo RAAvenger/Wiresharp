6f70ec71a71165fc61d91cbe123cdecae361c5d9
Reza Adibi <adbrezaooo@gmail.com>
2024-09-13 16:11:58 +0330

    Add project files.

---
+++ Wiresharp.Microsoft.Extensions.DependencyInjection/CodeGenerator.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using System.Text;
+using Wiresharp.Microsoft.Extensions.DependencyInjection.CodeTemplates;
+
+namespace Wiresharp.Microsoft.Extensions.DependencyInjection;
+
+[Generator]
+public class CodeGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        context.RegisterPostInitializationOutput(ctx => ctx.AddSource($"{DependencyInjectionAttributeCode.Attribute}.g.cs",
+            SourceText.From(DependencyInjectionAttributeCode.Attribute, Encoding.UTF8)));
+
+        // TODO: implement the remainder of the source generator
+    }
+}
---
+++ Wiresharp.Microsoft.Extensions.DependencyInjection/CodeTemplates/DependencyInjectionAttributeCode.cs
@@ -0,0 +1,15 @@
+﻿namespace Wiresharp.Microsoft.Extensions.DependencyInjection.CodeTemplates;
+
+internal class DependencyInjectionAttributeCode
+{
+    public const string ClassName = "DependencyInjectionAttribute";
+    public const string Attribute = $$"""
+        namespace Wiresharp
+        {
+            [System.AttributeUsage(System.AttributeTargets.Class)]
+            public sealed class {{ClassName}} : System.Attribute
+            {
+            }
+        }
+        """;
+}
